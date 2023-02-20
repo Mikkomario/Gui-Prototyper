ce605c0c63ca8e13162ff1ed20dd1bece052b513
Mikko Hilpinen <mikko_hilpinen@hotmail.com>
2023-02-20 16:52:35 +0200

    Prototyper: Testing the main view

--- src/vf/prototyper/app/GuiPrototyperApp.scala
+++ src/vf/prototyper/app/GuiPrototyperApp.scala
@@ -2,8 +2,11 @@ package vf.prototyper.app
 
 import utopia.paradigm.generic.ParadigmDataType
 import utopia.reflection.container.stack.StackHierarchyManager
+import vf.prototyper.model.immutable.Project
 import vf.prototyper.util.Common._
-import vf.prototyper.view.vc.StartProjectEditVc
+import vf.prototyper.view.vc.{MainVc, StartProjectEditVc}
+
+import scala.util.{Failure, Success}
 
 /**
  * @author Mikko Hilpinen
@@ -13,6 +16,33 @@ object GuiPrototyperApp extends App
 {
 	ParadigmDataType.setup()
 	
+	val initialProjects = Project.loadAll() match {
+		case Success((failures, projects)) =>
+			failures.headOption.foreach { log(_, s"Failed to open ${failures.size} projects") }
+			projects.sortBy { _.name }
+		case Failure(error) =>
+			log(error, "Project-loading failed")
+			Vector()
+	}
+	
 	StackHierarchyManager.startRevalidationLoop()
-	new StartProjectEditVc().display().foreach { _ => System.exit(0) }
+	
+	// Case: No projects to start with => Goes directly to new project view
+	if (initialProjects.isEmpty)
+		new StartProjectEditVc().display().foreach {
+			case Some(project) =>
+				new MainVc(Vector(project)).display().foreach { _ =>
+					println("Quitting")
+					System.exit(0)
+				}
+			case None =>
+				println("Quitting")
+				System.exit(0)
+		}
+	// Case: Projects available => Allows the user to choose the next action
+	else
+		new MainVc(initialProjects).display().foreach { _ =>
+			println("Quitting")
+			System.exit(0)
+		}
 }
--- src/vf/prototyper/view/vc/MainVc.scala
+++ src/vf/prototyper/view/vc/MainVc.scala
@@ -1,48 +1,42 @@
 package vf.prototyper.view.vc
 
 import utopia.flow.operator.EqualsFunction
-import utopia.flow.view.immutable.eventful.Fixed
+import utopia.flow.view.mutable.async.Volatile
 import utopia.flow.view.mutable.eventful.PointerWithEvents
+import utopia.paradigm.enumeration.Alignment.TopLeft
 import utopia.reach.component.button.image.ImageButton
 import utopia.reach.component.factory.Mixed
 import utopia.reach.component.label.text.TextLabel
 import utopia.reach.component.wrapper.Open
 import utopia.reach.container.ReachCanvas
-import utopia.reach.container.multi.stack.{MutableStack, Stack, ViewStack}
+import utopia.reach.container.multi.stack.{MutableStack, Stack}
 import utopia.reach.container.wrapper.Framing
-import utopia.reflection.component.context.TextContext
 import utopia.reflection.component.drawing.immutable.BackgroundDrawer
 import utopia.reflection.container.stack.StackLayout.Center
+import utopia.reflection.container.swing.window.Frame
+import utopia.reflection.container.swing.window.WindowResizePolicy.Program
 import utopia.reflection.controller.data.ContainerContentDisplayer2
-import utopia.reflection.shape.stack.StackLength
 import utopia.reflection.shape.LengthExtensions._
 import vf.prototyper.model.immutable.Project
 import vf.prototyper.util.Common._
 import vf.prototyper.view.Icon
 import vf.prototyper.view.component.ProjectRow
 
-import scala.util.{Failure, Success}
-
 /**
  * The main view-controller. Used for selecting the target project and for starting new projects.
  * @author Mikko Hilpinen
  * @since 15.2.2023, v0.1
  */
-class MainVc
+class MainVc(initialProjects: Vector[Project])
 {
 	// ATTRIBUTES   -------------------------
 	
-	private val projectsPointer = new PointerWithEvents(Project.loadAll() match {
-		case Success((failures, projects)) =>
-			failures.headOption.foreach { log(_, s"Failed to open ${ failures.size } projects") }
-			projects.sortBy { _.name }
-		case Failure(error) =>
-			log(error, "Project-loading failed")
-			Vector()
-	})
+	private val openProjectIdsPointer = Volatile(Set[String]())
+	
+	private val projectsPointer = new PointerWithEvents(initialProjects)
 	
 	// The view consists of a header and a projects -list
-	private val (view, projectsStack) = ReachCanvas(cursors) { hierarchy =>
+	private val view = ReachCanvas(cursors) { hierarchy =>
 		Stack(hierarchy).withContext(context.base).build(Framing).withoutMargin() { framingF =>
 			implicit def canvas: ReachCanvas = hierarchy.top
 			// Header
@@ -75,16 +69,60 @@ class MainVc
 				}
 			}
 			
-			Vector(header.parent, projectsFrame) -> projectsStack
+			Vector(header.parent, projectsFrame)
 		}
-	}.parentAndResult
+	}.parent
+	
+	// TODO: Remove test prints
+	openProjectIdsPointer.addContinuousListener { e => println(s"${ e.newValue.size } projects open") }
+	
+	
+	// COMPUTED --------------------------
+	
+	private def projects = projectsPointer.value
+	private def projects_=(newProjects: Vector[Project]) = projectsPointer.value = newProjects
 	
 	
 	// OTHER    --------------------------
 	
-	private def newProject() = ???
+	/**
+	 * Displays the main view
+	 * @return Future that resolves once all resulting windows have closed
+	 */
+	def display() = {
+		val frame = Frame.windowed(view, "GUI-Prototyper", Program, margins.medium, getAnchor = TopLeft.origin)
+		frame.startEventGenerators(actorHandler)
+		frame.visible = true
+		frame.closeFuture.flatMap { _ =>
+			openProjectIdsPointer.findMapFuture { p => if (p.nonEmpty) Some(()) else None }
+		}
+	}
 	
-	private def viewProject(project: Project) = ???
-	private def editProject(project: Project) = ???
-	private def deleteProject(project: Project) = ???
+	private def newProject() = new StartProjectEditVc().display().foreach { _.foreach { project =>
+		projectsPointer.update { p => (p :+ project).sortBy { _.name } }
+	} }
+	
+	// TODO: Implement
+	private def viewProject(project: Project) = println("View")
+	private def editProject(project: Project) = {
+		// Only allows a single edit view per project at a time
+		val shouldOpen = openProjectIdsPointer.pop { ids =>
+			if (ids.contains(project.id))
+				false -> ids
+			else
+				true -> (ids + project.id)
+		}
+		if (shouldOpen)
+			new StartProjectEditVc(Some(project)).display().foreach { result =>
+				result.foreach { newVersion =>
+					projectsPointer
+						.update { p => (p.filterNot { _.id == newVersion.id } :+ newVersion).sortBy { _.name } }
+				}
+				openProjectIdsPointer.update { _ - project.id }
+			}
+	}
+	private def deleteProject(project: Project) = {
+		if (!openProjectIdsPointer.value.contains(project.id))
+			projectsPointer.update { _.filterNot { _.id == project.id } }
+	}
 }
